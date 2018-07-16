d9ed30ada6a89f6d4aaf5965c1ef1b17a8bf5091
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-07-16 16:51:28 +0300

    Create Books.java

---
+++ Books.java
@@ -0,0 +1,79 @@
+package Task;
+
+public class Books {
+    private String title;
+    private String author;
+    private int year;
+    private double price;
+
+    public Books() {
+    }
+
+    public Books(String title, String author, int year, double price) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(price);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + year;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Books other = (Books) obj;
+        if (author == null) {
+            if (other.author != null)
+                return false;
+        } else if (!author.equals(other.author))
+            return false;
+        if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (year != other.year)
+            return false;
+        return true;
+    }
+
+
+}
